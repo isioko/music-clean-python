74edd8f64076a6af76f0cb2c8614652d2c09883d
ioko7 <isi.okojie@gmail.com>
2020-03-29 14:23:01 -0500

    add config

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def start():

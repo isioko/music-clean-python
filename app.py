1a67044189f403facf9b68311ca2d5391a8ed4bc
ioko7 <isi.okojie@gmail.com>
2020-04-11 21:55:04 -0500

    use flask sessions to store data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import requests 
 import base64 
 import time
@@ -17,14 +17,9 @@ user_playlists = classes.Playlists()
 
 debug = True
 
-def startup():
-	clean.reset()
-	user_playlists.reset()
-
 @app.route("/", methods=["GET", "POST"])
 def start():
 	if request.method == "GET":
-		startup()
 		session["token"] = None
 		session["username"] = None
 
@@ -32,7 +27,6 @@ def start():
 			print("ROUTE: START")
 			print("USERNAME (should be None):", session.get("username"))
 			print("TOKEN (should be None):", session.get("token"))
-			# print("PLAYLISTS (should be []):", user_playlists.playlists_list)
 
 		return render_template("home.html", auth_url=musicclean.get_authorize_url())
 
@@ -62,9 +56,6 @@ def getToken(code):
 
 		session["token"] = token
 		session["token_info"] = token_info
-		
-		# clean.setToken(token)
-		# clean.setTokenInfo(token_info)
 	else:
 		print("Error retrieving token")
 
@@ -74,30 +65,34 @@ def playlists():
 	if request.method == "GET":
 		playlists_dict = musicclean.getPlaylists(session.get("username"), session.get("token"))
 		session["playlists_dict"] = playlists_dict
-		# user_playlists.setPlaylistsDict(playlists_dict)
-
+		
 		playlists_list = []
 		for playlist in playlists_dict:
 			playlists_list.append(playlist)
 
 		session["playlists_list"] = playlists_list
 		session["num_playlists"] = len(playlists_list)	
-		# user_playlists.setPlaylistsList(playlists_list)
-		# user_playlists.setNumPlaylists(len(playlists_list))
-
+		
 		if debug:
 			print("ROUTE: PLAYLISTS GET")
-			# print("playlists_dict", user_playlists.playlists_dict, "playlists_list", user_playlists.playlists_list, "num_playlists", user_playlists.num_playlists)
-		
-		return render_template("playlists.html", playlists=playlists_list)
+			
+		return render_template("playlists.html", playlists=playlists_list, validNum="True")
 
 	if request.method == "POST":
 		if debug:
 			print("ROUTE: PLAYLISTS POST")
-			# print("playlists_dict", user_playlists.playlists_dict, "playlists_list", user_playlists.playlists_list, "num_playlists", user_playlists.num_playlists)
-			# print("playlist_to_clean_num", playlist_to_clean_num)
 		
-		playlist_to_clean_num = int(request.form['playlistToClean']) # need to check if can be converted to int
+		while True:	
+			playlists_list = session["playlists_list"]
+			try:
+				playlist_to_clean_num = int(request.form['playlistToClean']) # need to check if can be converted to int
+			except ValueError:
+				return render_template("playlists.html", playlists=playlists_list, validNum="False")
+			
+			if playlist_to_clean_num > session.get("num_playlists"):
+				return render_template("playlists.html", playlists=playlists_list, validNum="False")
+			else:
+				break
 		
 		if session.get("playlists_list") is not None:
 			playlists_list = session.get("playlists_list")
@@ -118,15 +113,6 @@ def playlists():
 		session["playlist_to_clean_name"] = playlist_to_clean_name
 		session["playlist_to_clean_id"] = playlist_to_clean_id
 
-		# user_playlists.setPlaylistToCleanNum(playlist_to_clean_num)
-		# user_playlists.setPlaylistToCleanID(user_playlists.playlists_dict[user_playlists.playlists_list[playlist_to_clean_num-1]][0])
-		# user_playlists.setPlaylistToCleanName(playlist_to_clean_name)
-
-		# clean_playlist_id = musicclean.createPlaylist(clean.username, clean.token, playlist_to_clean_name)
-		# _, all_tracks, could_not_clean_tracks = musicclean.getTracks(clean.username, clean.token, playlist_to_clean_name, user_playlists.playlists_dict[playlist_to_clean_name][0], clean_playlist_id, user_playlists.playlists_dict[playlist_to_clean_name][1])
-		# user_playlists.setAllTracks(all_tracks)
-		# user_playlists.setCouldNotCleanTracks(could_not_clean_tracks)
-
 		clean_playlist_id = musicclean.createPlaylist(session.get("username"), session.get("token"), playlist_to_clean_name)
 
 		playlists_dict = session.get("playlists_dict")
@@ -138,7 +124,6 @@ def playlists():
 
 @app.route("/cleanedplaylist/", methods=["GET"])
 def cleanedPlaylist():
-	# return render_template("cleaned_playlist.html", playlistName=user_playlists.playlist_to_clean_name, allTracks=user_playlists.all_tracks, notCleanTracks=user_playlists.could_not_clean_tracks)
 	playlist_to_clean_name = session.get("playlist_to_clean_name")
 	all_tracks = session.get("all_tracks")
 	could_not_clean_tracks = session.get("could_not_clean_tracks")
@@ -148,7 +133,6 @@ def cleanedPlaylist():
 
 def getUsername():
 	headers = {
-		# "Authorization": "Bearer " + clean.token,
 		"Authorization": "Bearer " + session.get("token"),
 	}
 
@@ -158,7 +142,6 @@ def getUsername():
 	if response.status_code == 200:
 		user = response.json()
 		session["username"] = user["id"]
-		# clean.setUsername(user['id'])
 	else:
 		print("Error retrieving username")
 
@@ -167,23 +150,12 @@ def callback():
 	if debug:
 		print("ROUTE: CALLBACK")
 
-	# if clean.token is None:
-	# if session.get("token") is None:
 	getToken(request.args['code'])
 
 	if debug:
 		print("Got new token:", session.get("token"))
 
-	#elif clean.token is not None and is_token_expired(clean.token_info):
-	# elif session.get("token") is not None and is_token_expired(session.get("token_info")):
-	# 	# refresh token here
-	# 	print("need to refresh")
-
-	# 	if debug:
-	# 		print("Needed to refresh token")
-
 	getUsername()
-	print("yay")
 
 	if debug:
 		print("Got username:", session.get("username"))

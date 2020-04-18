3f03b6a228700c087371568b7828ed178a993b2c
ioko7 <isi.okojie@gmail.com>
2020-04-18 15:51:57 -0500

    fix redirect uri

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
 CLIENT_ID = "4ae5e0aa7e9544308505ab72bf04af06"
 CLIENT_SECRET = "f53edb5d2c3a4b7da4d287a85ec77b2e"
-REDIRECT_URI = "https://music-clean-pro.herokuapp.com/callback/"
+REDIRECT_URI = "https://music-clean-stage.herokuapp.com/callback/"
 # REDIRECT_URI = "http://127.0.0.1:5000/callback/"

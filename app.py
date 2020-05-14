4b626a2230248e98970663602d2f464767cb9da1
ioko7 <isi.okojie@gmail.com>
2020-05-13 19:10:51 -0500

    clean up code

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sys
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import requests 
-import base64 
 import time
 
 import musicclean
@@ -18,86 +17,57 @@ user_playlists = classes.Playlists()
 
 debug = True
 
+
+"""
+For reference: shows design elements for HTML/CSS template
+"""
 @app.route("/elements")
 def elements():
 	return render_template("elements.html")
 
+
+"""
+Homepage of app
+"""
 @app.route("/", methods=["GET", "POST"])
 def start():
 	if request.method == "GET":
 		session["token"] = None
 		session["username"] = None
 
-		if debug:
-			print("ROUTE: START")
-			print("USERNAME (should be None):", session.get("username"))
-			print("TOKEN (should be None):", session.get("token"))
-
 		return render_template("home.html", auth_url=musicclean.get_authorize_url())
 
-def is_token_expired(token_info):
-    now = int(time.time())
-    return token_info["expires_in"] - now < 60
 
-def make_authorization_headers(client_id, client_secret):
-	auth_str = client_id + ":" + client_secret
-	auth_header = base64.b64encode(auth_str.encode('ascii'))
-	return {"Authorization": "Basic %s" % auth_header.decode('ascii')}
-
-def getToken(code):
-	payload = {
-		"redirect_uri": secrets.REDIRECT_URI,
-		"code": code,
-		"grant_type": "authorization_code"
-	}
-
-	headers = make_authorization_headers(secrets.CLIENT_ID, secrets.CLIENT_SECRET)
-	url = "https://accounts.spotify.com/api/token"
-
-	response = requests.post(url, data=payload, headers=headers)
-	if response.status_code == 200:
-		token_info = response.json()
-		token = token_info['access_token']
-
-		session["token"] = token
-		session["token_info"] = token_info
-	else:
-		print("Error retrieving token")
-
-def getPlaylists():
-	playlists_dict = musicclean.getPlaylists(session.get("username"), session.get("token"))
-
-	return playlists_dict
-
+"""
+Shows a list of all of user's Spotify playlists and allows user to select one to clean
+"""
 @app.route("/playlists/", methods=["GET", "POST"])
 def playlists():
 	if request.method == "GET":
-		if debug:
-			print("ROUTE: PLAYLISTS GET")
-
-		playlists_dict = getPlaylists()
+		playlists_dict = musicclean.get_playlists(session.get("username"), session.get("token"))
 			
 		return render_template("playlists.html", playlists=playlists_dict, validNum="True")
 
 	if request.method == "POST":
-		if debug:
-			print("ROUTE: PLAYLISTS POST")
-
 		playlist_to_clean_id = request.form['select-playlist']
 		session["playlist_to_clean_id"] = playlist_to_clean_id
 
-		return redirect(url_for('cleanedPlaylist'))
+		return redirect(url_for('cleaned_playlist'))
 
+
+"""
+Shows user a list of songs added to new playlist as well as a list of songs without a cleaned version
+"""
 @app.route("/cleanedplaylist/", methods=["GET"])
-def cleanedPlaylist():
-	playlists_dict = getPlaylists()
+def cleaned_playlist():
+	playlists_dict = musicclean.get_playlists(session.get("username"), session.get("token"))
 
 	playlist_to_clean_id = session.get("playlist_to_clean_id")
 	playlist_to_clean_name = playlists_dict[playlist_to_clean_id]
 
-	clean_playlist_id = musicclean.createPlaylist(session.get("username"), session.get("token"), playlist_to_clean_name)
+	clean_playlist_id = musicclean.create_playlist(session.get("username"), session.get("token"), playlist_to_clean_name)
 
-	_, all_tracks, could_not_clean_tracks = musicclean.getTracks(session.get("username"), session.get("token"), playlist_to_clean_name, playlist_to_clean_id, clean_playlist_id, False)
+	_, all_tracks, could_not_clean_tracks = musicclean.get_tracks(session.get("username"), session.get("token"), playlist_to_clean_name, playlist_to_clean_id, clean_playlist_id)
 
 	clean_tracks = list(set(all_tracks).difference(set(could_not_clean_tracks)))
 
@@ -105,38 +75,19 @@ def cleanedPlaylist():
 	if len(could_not_clean_tracks) > 0:
 		has_uncleaned_tracks = "True"
 
-	return render_template("cleaned_playlist.html", playlistName=playlist_to_clean_name,  cleanTracks=clean_tracks, notCleanTracks=could_not_clean_tracks, hasNotClean = has_uncleaned_tracks)
+	return render_template("cleaned_playlist.html", playlistName=playlist_to_clean_name, cleanTracks=clean_tracks, notCleanTracks=could_not_clean_tracks, hasNotClean=has_uncleaned_tracks)
 
-def getUsername():
-	headers = {
-		"Authorization": "Bearer " + session.get("token"),
-	}
 
-	url = "https://api.spotify.com/v1/me"
-
-	response = requests.get(url, headers=headers)
-	if response.status_code == 200:
-		user = response.json()
-		session["username"] = user["id"]
-	else:
-		print("Error retrieving username")
-
+"""
+Used to catch callback from Spotify API to get authorization code
+"""
 @app.route("/callback/", methods=["GET"])
 def callback():
-	if debug:
-		print("ROUTE: CALLBACK")
-
-	getToken(request.args['code'])
-
-	if debug:
-		print("Got new token:", session.get("token"))
-
-	getUsername()
-
-	if debug:
-		print("Got username:", session.get("username"))
+	musicclean.get_token(request.args['code'])
+	musicclean.get_username()
 
 	return redirect(url_for('playlists'))
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=debug)
